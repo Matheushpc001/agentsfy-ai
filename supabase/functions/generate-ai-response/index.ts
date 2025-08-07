89d7c92f4ddcbb219bf4c69006be81286bd5cf5e
ANDREW <102931168+AndrewBait@users.noreply.github.com>
2025-08-07 10:51:25 -0300

    Update index.ts

--- supabase/functions/generate-ai-response/index.ts
+++ supabase/functions/generate-ai-response/index.ts
@@ -1,5 +1,5 @@
 // ARQUIVO: supabase/functions/generate-ai-response/index.ts
-
+//test
 import "https://deno.land/x/xhr@0.1.0/mod.ts";
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
